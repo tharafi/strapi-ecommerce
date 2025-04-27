d98e475bcf1426999edacccd108b41708f1e656f
tharafi sliman <tharafisliman6@gmail.com>
2025-04-27 18:56:07 +0100

    edit in middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,13 +2,7 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
   {
     name: 'strapi::cors',
     config: {
@@ -17,8 +11,6 @@ module.exports = [
     },
   },
   'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
